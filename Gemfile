20713c6dfa61ecf4b3b02500ceaa55fc95bf30fd
Andras Kerekes <andras.kerekes.work@gmail.com>
2014-11-06 13:52:49 -0500

    added install_dist recipe, and attributes for version 3.1.1 for dist

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'berkshelf', github: 'berkshelf/berkshelf'
+gem 'foodcritic'
+gem 'rubocop'
+gem 'rubocop-checkstyle_formatter'
+gem 'chefspec'
+gem 'serverspec'
+gem 'berkflow'
+
+gem 'chef', '~> 11.12.2'
+
+group :integration do
+  gem 'test-kitchen'
+  gem 'kitchen-vagrant'
+  gem 'kitchen-docker'
+  gem 'kitchen-ec2', github: 'sailthru-cookbooks/kitchen-ec2'
+end
---
+++ attributes/dist.rb
@@ -0,0 +1,51 @@
+default['r']['dist'] = {
+  "3.1.1" => {
+    'r-recommended' => '3.1.1-1trusty0',
+    "r-doc-html" => "3.1.1-1trusty0",
+    "r-cran-survival" => "2.37-7-1",
+    "r-cran-spatial" => "7.3-7-1trusty0",
+    "r-cran-rpart" => "4.1-8-1trusty0",
+    "r-cran-nnet" => "7.3-8-1trusty0",
+    "r-cran-nlme" => "3.1.117-1trusty0",
+    "r-cran-mgcv" => "1.8-3-1trusty0",
+    "r-cran-matrix" => "1.1-4-1trusty0",
+    "r-cran-mass" => "7.3-34-1trusty0",
+    "r-cran-lattice" => "0.20-29-1trusty0",
+    "r-cran-kernsmooth" => "2.23-13-1trusty0",
+    "r-cran-foreign" => "0.8.61-1trusty0",
+    "r-cran-codetools" => "0.2-9-1trusty0",
+    "r-cran-cluster" => "1.15.3-1trusty0",
+    "r-cran-class" => "7.3-11-1trusty0",
+    "r-cran-boot" => "1.3-13-1trusty0",
+    "r-base-html" => "3.1.1-1trusty0",
+    "r-base-dev" => "3.1.1-1trusty0",
+    "r-base-core" => "3.1.1-1trusty0",
+    "r-base" => "3.1.1-1trusty0"
+  }
+}
+
+
+
+
+#default['r']['dist']['packages'] = [
+#  { :name => 'r-base', :version => '3.1.1-1trusty0' },
+#  { :name => 'r-base-core', :version => '3.1.1-1trusty0' },
+#  { :name => 'r-base-dev', :version => '3.1.1-1trusty0' },
+#  { :name => 'r-base-html', :version => '3.1.1-1trusty0' },
+#  { :name => 'r-cran-boot', :version => '1.3-13-1trusty0' },
+#  { :name => 'r-cran-class', :version => '7.3-11-1trusty0' },
+#  { :name => 'r-cran-cluster', :version => '1.15.3-1trusty0' },
+#  { :name => 'r-cran-codetools', :version => '0.2-9-1trusty0' },
+#  { :name => 'r-cran-foreign', :version => '0.8.61-1trusty0' },
+#  { :name => 'r-cran-kernsmooth', :version => '2.23-13-1trusty0' },
+#  { :name => 'r-cran-lattice', :version => '0.20-29-1trusty0' },
+#  { :name => 'r-cran-mass', :version => '7.3-34-1trusty0' },
+#  { :name => 'r-cran-matrix', :version => '1.1-4-1trusty0' },
+#  { :name => 'r-cran-mgcv', :version => '1.8-3-1trusty0' },
+#  { :name => 'r-cran-nlme', :version => '3.1.117-1trusty0' },
+#  { :name => 'r-cran-nnet', :version => '7.3-8-1trusty0' },
+#  { :name => 'r-cran-rpart', :version => '4.1-8-1trusty0' },
+#  { :name => 'r-cran-spatial', :version => '7.3-7-1trusty0' },
+#  { :name => 'r-cran-survival', :version => '2.37-7-1' },
+#  { :name => 'r-doc-html', :version => '3.1.1-1trusty0' },
+#  { :name => 'r-recommended', :version => '3.1.1-1trusty0' } ]
