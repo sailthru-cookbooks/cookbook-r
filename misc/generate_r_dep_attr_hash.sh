10435d0c9ae4727c8290d5eec018c7a72242f338
Andras Kerekes <andras.kerekes.work@gmail.com>
2014-11-07 13:11:03 -0500

    fixed typos

--- misc/generate_r_dep_attr_hash.sh
+++ misc/generate_r_dep_attr_hash.sh
@@ -1,4 +1,4 @@
-# run this script to generate a attirbute hash of r dependency packages with <name> => <version> pairs that can be pasted into
+# run this script to generate an attribute hash of r dependency packages with <name> => <version> pairs that can be pasted into
 # the chef attributes file. You may want to change the version string to the actual version of R on the system. It is set to 
 # 3.1.2 currently (and this assumes the node where the command is executed has R 3.1.2 installed
 
