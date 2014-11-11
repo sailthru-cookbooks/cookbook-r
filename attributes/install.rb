8c2562e8dded37e489cd1a19011cdab9ce7657a6
akerekes <andras.kerekes.work@gmail.com>
2014-11-11 10:16:45 -0500

    added install dir for "dist" installation

--- attributes/install.rb
+++ attributes/install.rb
@@ -6,4 +6,6 @@ when "package"
   default['r']['install_dir'] = "/usr/lib/R"
 when "source"
   default['r']['install_dir'] = node['kernel']['machine'] == 'x86_64' ? "/usr/local/lib64/R" : "/usr/local/lib/R"
+when "dist"
+  default['r']['install_dir'] = node['kernel']['machine'] == 'x86_64' ? "/usr/local/lib64/R" : "/usr/local/lib/R"
 end
